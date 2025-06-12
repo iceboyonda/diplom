2a1e2f07c8a3d0a3c0906a7a018de198fc2091e3
dnd <120388523+iceboyonda@users.noreply.github.com>
2025-06-12 19:39:50 +0300

    123

--- tyre_trust/settings.py
+++ tyre_trust/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['94.241.139.148', 'localhost', '127.0.0.1', 'tyretrust.ru', 'www.tyretrust.ru']
 
